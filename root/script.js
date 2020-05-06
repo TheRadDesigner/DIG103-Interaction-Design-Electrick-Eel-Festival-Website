58f9395858212835175506c6f2763724eb599310
Luke Stewart <47406546+TheRadDesigner@users.noreply.github.com>
2020-05-07 04:34:43 +1000

    Made Mobile Res

---
+++ root/script.js
@@ -0,0 +1,7 @@
+$(".HamburgerIcon").on("click", function(){
+  $(".alt-menu").toggleClass("alt-menu-open");
+});
+
+$(".alt-hamburger").on("click", function(){
+  $(".alt-menu").toggleClass("alt-menu-open");
+});
